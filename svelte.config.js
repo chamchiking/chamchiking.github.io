610f10a9d22a1af9f1f1351e46be0b117593bab7
차민철 <mccha@chamincheol-ui-MacBookPro.local>
2024-12-31 04:51:06 +0900

    fix: changed fallback location

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'none' // Fallback for unmatched routes
+			fallback: 'index.html' // Fallback for unmatched routes
 		}),
 		prerender: {
-			entries: ['*'], // Attempt to prerender all pages
-			handleHttpError: ({ status, path, referrer }) => {
-				if (status === 404) {
-					console.warn(`404 error at ${path} (referred from ${referrer})`);
-					return;
-				}
-				throw new Error(`${status} error at ${path}`);
-			}
+			entries: ['*'] // Attempt to prerender all pages
 		},
 		paths: {
-			base: dev ? '' : '' // Replace with your repository name
+			base: '' // Replace with your repository name
 		}
 	}
 };
